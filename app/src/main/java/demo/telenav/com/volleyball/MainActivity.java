ab2e7b8acd6a6e0701b97d1e10ae18e45f749752
duanbo1983 <duanbo1983@gmail.com>
2015-01-26 15:04:53 -0800

    3 ways to load image with Volley.

--- app/src/main/java/demo/telenav/com/volleyball/MainActivity.java
+++ app/src/main/java/demo/telenav/com/volleyball/MainActivity.java
@@ -1,15 +1,20 @@
 package demo.telenav.com.volleyball;
 
+import android.graphics.Bitmap;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.ImageRequest;
+import com.android.volley.toolbox.NetworkImageView;
 import com.android.volley.toolbox.StringRequest;
 
 
@@ -30,7 +35,7 @@ public class MainActivity extends ActionBarActivity {
                     @Override
                     public void onResponse(String response) {
                         // Display the first 500 characters of the response string.
-                        mTextView.setText("Response is: "+ response.substring(0,500));
+                        mTextView.setText("Response is: "+ response.substring(0,10));
                     }
                 }, new Response.ErrorListener() {
             @Override
@@ -40,6 +45,40 @@ public class MainActivity extends ActionBarActivity {
         });
         // Add the request to the RequestQueue.
         queue.add(stringRequest);
+
+
+        final ImageView mImageView = (ImageView) findViewById(R.id.myImage);
+        url = "http://i.imgur.com/7spzG.png";
+
+        // Retrieves an image specified by the URL, displays it in the UI.
+        ImageRequest request = new ImageRequest(url,
+                new Response.Listener<Bitmap>() {
+                    @Override
+                    public void onResponse(Bitmap bitmap) {
+                        mImageView.setImageBitmap(bitmap);
+                    }
+                }, 0, 0, null,
+                new Response.ErrorListener() {
+                    public void onErrorResponse(VolleyError error) {
+                        mImageView.setImageResource(R.drawable.ic_launcher);
+                    }
+                });
+        // Access the RequestQueue through your singleton class.
+        queue.add(request);
+
+        ImageLoader mImageLoader = RequestQueueManager.getInstance(getApplicationContext()).getImageLoader();
+        ImageView mImageView2 = (ImageView) findViewById(R.id.myImage2);
+
+        String IMAGE_URL = "http://developer.android.com/images/training/system-ui.png";
+        // Get the ImageLoader through your singleton class.
+        mImageLoader.get(IMAGE_URL, ImageLoader.getImageListener(mImageView2,
+                R.drawable.ic_launcher, R.drawable.ic_launcher));
+
+        NetworkImageView mNetworkImageView = (NetworkImageView) findViewById(R.id.networkImageView);
+
+        // Set the URL of the image that should be loaded into this view, and
+        // specify the ImageLoader that will be used to make the request.
+        mNetworkImageView.setImageUrl(IMAGE_URL, mImageLoader);
     }
 
 
