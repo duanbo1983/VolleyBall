ae0473f8ebc9b5d5b1aaf02fa87c994c200bb35d
duanbo1983 <duanbo1983@gmail.com>
2015-01-26 14:35:53 -0800

    Add simple test

--- app/src/main/java/demo/telenav/com/volleyball/MainActivity.java
+++ app/src/main/java/demo/telenav/com/volleyball/MainActivity.java
@@ -4,6 +4,13 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,6 +19,27 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView mTextView = (TextView) findViewById(R.id.textView);
+        String url ="http://www.google.com";
+
+        RequestQueue queue = RequestQueueManager.getInstance(getApplicationContext()).getRequestQueue();
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        mTextView.setText("Response is: "+ response.substring(0,500));
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                mTextView.setText("That didn't work!");
+            }
+        });
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
     }
 
 
